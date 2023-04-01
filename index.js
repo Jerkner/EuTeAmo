45fcf13a2bf092e051733bf727403cf012c3ff71
PhiJer <philip@jerkner.se>
2023-04-01 18:21:58 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+clickMe = document.getElementById("click-me")
+container = document.getElementById("container")
+
+clickMe.addEventListener("click", function() {
+    container.innerHTML = `<h1>Eu te amo muito
+    <span class="muito1">muito</span>
+    <span class="muito2">muito</span>
+    <span class="muito3">muito!</span>
+    </h1>`
+})
